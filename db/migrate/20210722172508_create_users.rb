362084293c7a8cde50187e58f44da62a88719de2
anonaeka <anonvisualdesign@gmail.com>
2021-07-23 03:35:19 +1000

    deviseset

---
+++ db/migrate/20210722172508_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.text :bio
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
